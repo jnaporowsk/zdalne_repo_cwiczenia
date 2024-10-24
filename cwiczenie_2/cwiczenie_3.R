33ba25d480a1b6bfee80867bbb9db2d66de22a88
jnaporowsk <jnaporowsk@student.agh.edu.pl>
2024-10-24 19:12:00 +0200

    Wykonane cw 4.7 oraz plik quatro

--- cwiczenie_2/cwiczenie_3.R
+++ cwiczenie_2/cwiczenie_3.R
@@ -205,69 +205,170 @@ log_reg_wyniki <- rename(log_reg_wyniki, nazwa_metody = ...1)
 ## metoda lasu losowego
 
 rf_mod <- 
-  rand_forest(trees = 1000) |> 
+  rand_forest(trees = tune(),
+              mtry = tune()) |> 
   set_engine("ranger") |> 
   set_mode("classification")
+
+mtry_param <- finalize(mtry(), x = train_data)
+rf_mod
+
+siatka <- grid_regular(mtry_param,
+                       trees(), 
+                       levels = 5)
+siatka
+siatka |> count(mtry)
 
 
 rf_wflow <- workflow() |> 
   add_model(rf_mod) |> 
   add_recipe(air_recipe_o3) 
 
+miary_oceny <-
+  yardstick::metric_set(
+    accuracy,
+    mcc,
+    npv,
+    roc_auc)
+
+
+
 # cv fold
 set.seed("123")
 air_rf_folds = vfold_cv(train_data, v=10)
 
+library(doParallel)
+
+# Ustawienie liczby wątków równej liczbie rdzeni procesora
+cl <- makePSOCKcluster(parallel::detectCores() - 1)
+registerDoParallel(cl)
+
+air_fit_rf_folds <-
+  rf_wflow |>
+  tune_grid(
+    resamples = air_rf_folds,
+    grid = siatka,
+    metrics = miary_oceny
+  )
+
+stopCluster(cl)
+
 air_rf_folds$splits[[1]] %>% analysis() %>% dim()
 
-air_fit_rf_folds <- rf_wflow |> fit_resamples(air_rf_folds)
 air_fit_rf_folds |> 
   collect_metrics() |> 
   knitr::kable(digits = 3)
 
+air_fit_rf_folds |> select_best(metric = "accuracy")
+
+air_fit_rf_folds_best <- rf_wflow |> finalize_workflow(air_fit_rf_folds |> select_best(metric = "accuracy"))
+
+air_fit_rf_folds_final <- air_fit_rf_folds_best |> last_fit(split = data_split)
+
+air_fit_rf_folds_final |> 
+  collect_metrics() |> 
+  knitr::kable(digits = 3)
 
 # cv fold repeated
 set.seed("123")
 air_rf_folds_rep = vfold_cv(train_data, v=10, repeats = 5)
 
+# Ustawienie liczby wątków równej liczbie rdzeni procesora
+cl <- makePSOCKcluster(parallel::detectCores() - 1)
+registerDoParallel(cl)
+
+air_fit_rf_folds_rep <-
+  rf_wflow |>
+  tune_grid(
+    resamples = air_rf_folds_rep,
+    grid = siatka,
+    metrics = miary_oceny
+  )
+
+stopCluster(cl)
+
 air_rf_folds_rep$splits[[1]] %>% analysis() %>% dim()
 
-air_fit_rf_folds_rep <- rf_wflow |> fit_resamples(air_rf_folds_rep)
-air_fit_rf_folds_rep |> 
+air_fit_rf_folds_rep |> select_best(metric = "accuracy")
+
+air_fit_rf_folds_rep_best <- rf_wflow |> finalize_workflow(air_fit_rf_folds_rep |> select_best(metric = "accuracy"))
+
+air_fit_rf_folds_rep_final <- air_fit_rf_folds_rep_best |> last_fit(split = data_split)
+
+air_fit_rf_folds_rep_final |> 
   collect_metrics() |> 
   knitr::kable(digits = 3)
+
  
 
 # monte carlo cv
 set.seed("123")
-air_rf_mc = mc_cv(train_data, prop=9/10, times = 5)
-air_rf_mc
+air_rf_mc = mc_cv(train_data, prop=9/10, times = 10)
+
+cl <- makePSOCKcluster(parallel::detectCores() - 1)
+registerDoParallel(cl)
+
+air_fit_rf_mc <-
+  rf_wflow |>
+  tune_grid(
+    resamples = air_rf_mc,
+    grid = siatka,
+    metrics = miary_oceny
+  )
+
+stopCluster(cl)
+
 air_rf_mc$splits[[1]] %>% analysis() %>% dim()
 
-air_fit_rf_mc <- rf_wflow |> fit_resamples(air_rf_mc)
-air_fit_rf_mc |> 
+air_fit_rf_mc |> select_best(metric = "accuracy")
+
+air_fit_rf_mc_best <- rf_wflow |> finalize_workflow(air_fit_rf_mc |> select_best(metric = "accuracy"))
+
+air_fit_rf_mc_final <- air_fit_rf_mc_best |> last_fit(split = data_split)
+
+air_fit_rf_mc_final |> 
   collect_metrics() |> 
   knitr::kable(digits = 3)
+
 
 
 # bootstrap
-air_rf_bootstrap <- bootstraps(train_data, times = 5)
-air_rf_bootstrap
+set.seed("123")
+air_rf_bootstrap <- bootstraps(train_data, times = 10)
+cl <- makePSOCKcluster(parallel::detectCores() - 1)
+registerDoParallel(cl)
+
+air_fit_rf_bootstrap <-
+  rf_wflow |>
+  tune_grid(
+    resamples = air_rf_bootstrap,
+    grid = siatka,
+    metrics = miary_oceny
+  )
+
+stopCluster(cl)
+
 air_rf_bootstrap$splits[[1]] %>% analysis() %>% dim()
 
-air_fit_rf_bootstrap <- final_model |> fit_resamples(air_rs_bootstrap)
-air_fit_rf_bootstrap |> 
+air_fit_rf_bootstrap |> select_best(metric = "accuracy")
+
+air_fit_rf_bootstrap_best <- rf_wflow |> finalize_workflow(air_fit_rf_bootstrap |> select_best(metric = "accuracy"))
+
+air_fit_rf_bootstrap_final <- air_fit_rf_bootstrap_best |> last_fit(split = data_split)
+
+air_fit_rf_bootstrap_final |> 
   collect_metrics() |> 
   knitr::kable(digits = 3)
+
 
 rf_wyniki <- bind_cols(
   c(rep("Vfold", 3), rep("Vfold-repeated",3), rep("Monte carlo cv",3), rep("Bootstrap", 3)),
   
   bind_rows(
-    air_fit_rf_folds |> collect_metrics() ,
-    air_fit_rf_folds_rep |> collect_metrics() ,
-    air_fit_rf_mc |> collect_metrics() ,
-    air_fit_rf_bootstrap |> collect_metrics() 
+    air_fit_rf_folds_final |> collect_metrics() ,
+    air_fit_rf_folds_rep_final |> collect_metrics() ,
+    air_fit_rf_mc_final |> collect_metrics() ,
+    air_fit_rf_bootstrap_final |> collect_metrics() 
   ))|> tibble()
 
 rf_wyniki <- rename(rf_wyniki, nazwa_metody = ...1)
@@ -285,9 +386,9 @@ wykres_log_reg <- ggplot(log_reg_wyniki, aes(x = nazwa_metody, y = mean, fill = 
   theme_bw() +
   theme(axis.text.x = element_blank())
 
-wykres_rf <- ggplot(rf_wyniki, aes(x = nazwa_metody, y = mean, fill = nazwa_metody)) +
+wykres_rf <- ggplot(rf_wyniki, aes(x = nazwa_metody, y = .estimate, fill = nazwa_metody)) +
   geom_bar(stat = "identity", position = "dodge") +
-  geom_text(aes(label = round(mean, 3)), 
+  geom_text(aes(label = round(.estimate, 3)), 
             position = position_dodge(width = 0.9), 
             vjust = -0.5, size = 3) +
   labs(title = "Las losowy - Porównanie wyników w zależności od metody walidacji",
